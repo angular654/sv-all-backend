740f0d57195cf9f99054e49291a0fd48423420aa
DaEPopov <pde-rnd@yandex.ru>
2021-10-15 15:29:25 +0300

    Add Dockerfile and env vars

--- client/Redis.py
+++ client/Redis.py
@@ -1,10 +1,17 @@
 import redis
 import json
+from decouple import config
+
+REDIS_HOST = config('REDIS_HOST')
+REDIS_PORT = config('REDIS_PORT')
+REDIS_DB = config('REDIS_DB')
 class Redis:
     client = ''
     def __init__(self):
-        self.client = redis.StrictRedis(host='localhost', port=6379, db=0)
+        self.client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
     def send_data(self, data: str):
         self.client.publish('logger',data)
     def read_configs(self):
-        return json.loads(self.client.get('stored_config').decode('utf-8'))
+        stored_config = self.client.get('stored_config')
+        if stored_config:
+            return json.loads(stored_config.decode('utf-8'))
--- logs-service/snake-vision-sockets-consumer/app.py
+++ logs-service/snake-vision-sockets-consumer/app.py
@@ -4,6 +4,8 @@ from flask_sockets import Sockets
 from  RedisListener import PubSubListener
 from RedisListener import RedisChache
 import requests
+from decouple import config
+
 pslistener = PubSubListener()
 db = RedisChache()
 app = Flask(__name__)
@@ -14,6 +16,10 @@ def echo_socket(ws):
     while not ws.closed:
         gevent.sleep(0.1)
 
+SV_HISTORY_HOST = config('SV_HISTORY_HOST')
+SV_HISTORY_PORT = config('SV_HISTORY_PORT')
+LOGS_SERVICE_PORT = config('LOGS_SERVICE_PORT')
+NODE_SV_HISTORY = f'http://{SV_HISTORY_HOST}:{SV_HISTORY_PORT}'
 
 @app.route('/')
 def hello():
@@ -22,14 +28,14 @@ def hello():
 
 @app.route('/configs-history', methods=["GET"])
 def conf_history():
-    url = 'http://localhost:7070/configs'
+    url = f'{NODE_SV_HISTORY}/configs'
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     r = requests.get(url=url,headers=headers)
     return r
 
 @app.route('/events-history', methods=["GET"])
 def events_history():
-    url = 'http://localhost:7070/events'
+    url = f'{NODE_SV_HISTORY}/events'
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     r = requests.get(url=url,headers=headers)
     return r
@@ -38,5 +44,5 @@ if __name__ == "__main__":
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
     print("Started")
-    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler)
+    server = pywsgi.WSGIServer(('0.0.0.0', LOGS_SERVICE_PORT), app, handler_class=WebSocketHandler)
     server.serve_forever()
